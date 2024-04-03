71b46a23bd1aafc9eafa6aad4034475db61310b1
varungm <varungm@varungms-MacBook-Air.local>
2024-04-03 20:45:51 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,30 @@
+import img1 from './img/img1.png';
+import img2 from './img/img2.png';
+import img3 from './img/img3.png';
+
+const data= {
+    productData: [
+        {
+            id:1,
+            img:img1,
+            tittle:'Juisy Graphs',
+            desc:' ',
+            price:46,
+        },
+        {
+            id:2,
+            img:img2,
+            tittle:'Red watermelon',
+            desc:' ',
+            price:60, 
+        },
+        {
+            id:3,
+            img:img3,
+            tittle:'Juisy Orange',
+            desc:' ',
+            price:80,
+        }
+    ],
+};
+export default data;
